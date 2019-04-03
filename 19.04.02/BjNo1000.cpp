83bf696794442f0eea45af807cb2c08418c3751d
Min-92 <okssud1@naver.com>
2019-04-03 10:39:53 +0900

    백준 1000 A+B 풀이
    std:: >> using namespace std로 변경

--- 19.04.02/BjNo1000.cpp
+++ 19.04.02/BjNo1000.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 int main(){
-    using std::cout;
-    using std::cin;
+    // using std::cout;
+    // using std::cin;
+
+    using namespace std; //표준라이브러리 이용해서 빠른코딩
+
     int a, b, sum;
 
 

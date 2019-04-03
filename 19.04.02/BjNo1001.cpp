7e9d55bb9099230de50fbf68c31246a3b44626ca
Min-92 <okssud1@naver.com>
2019-04-03 10:40:25 +0900

    백준 1001 A-B 풀이
    std:: >> using namespace std로 변경

--- 19.04.02/BjNo1001.cpp
+++ 19.04.02/BjNo1001.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 int main(){
-    using std::cout;
-    using std::cin;
+    // using std::cout;
+    // using std::cin;
 
+    using namespace std; //표준라이브러리 이용해서 빠른코딩
     int a,b,sub;
 
     cin >> a >> b;

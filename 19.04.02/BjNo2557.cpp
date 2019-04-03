b0cf5e1c408c23e5759da1802740322e71ae7531
Min-92 <okssud1@naver.com>
2019-04-03 10:39:21 +0900

    백준 2557 Hello world 풀이
    std:: >> using namespace std로 변경

--- 19.04.02/BjNo2557.cpp
+++ 19.04.02/BjNo2557.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main(){
-    std::cout <<"Hello World!";
+    // using std::cout;
+    // using std::cin;
+
+    using namespace std; //표준라이브러리 이용해서 빠른코딩
+    // std::cout <<"Hello World!";
+    cout << "Hello World!";
     return 0;
 }

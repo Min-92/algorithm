55485a79efe222a7a0c9368bf3da54ec56c113f8
Min-92 <okssud1@naver.com>
2019-04-09 01:39:06 +0900

    백준 2439 별찍기 -2 풀이

---
+++ pg2439.cpp
@@ -0,0 +1,19 @@
+//백준 2439 별찍기 -2
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = n-i-1; j > 0; j--){
+            cout << " ";
+        }
+        for(int k = 0; k <= i; k++){
+            cout << "*";
+        }
+            cout << "\n";
+    }
+    return 0;
+}

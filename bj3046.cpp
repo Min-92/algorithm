49384845eff4b38379d1a1cf737f9417c07fb752
Min-92 <okssud1@naver.com>
2019-04-25 10:20:49 +0900

    Back Joon 3046 R2
    - 평균 반대로 구하기

---
+++ bj3046.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/3046
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cin >> a >>b ;
+
+    cout << 2*b-a;
+
+    return 0;
+}

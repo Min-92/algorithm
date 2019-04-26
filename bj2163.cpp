ffb11563e2e7e159163fcf2cb1dda8886313c25e
Min-92 <okssud1@naver.com>
2019-04-26 10:17:04 +0900

    Back Joon 2163 초콜릿 자르기
    - 입력,출력, 곱하기

---
+++ bj2163.cpp
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/2163
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a*b-1;
+
+
+
+    return 0;
+}

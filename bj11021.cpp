8c33851e0e0dab41eccc9dc28c2a9ea2662fa1ef
Min-92 <okssud1@naver.com>
2019-04-26 10:24:39 +0900

    Back Joon 11021 A+B -7
    - 입력 출력 for 문자열편집

---
+++ bj11021.cpp
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/problem/11021
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+
+    cin >> t;
+    int a,b;
+    for(int i = 0; i < t ; i++){
+        cin >> a >> b;
+
+        cout << "Case #"<<i+1<<": "<< a+b<<"\n";
+    }
+
+    return 0;
+}

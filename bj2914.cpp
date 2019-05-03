15fa3e4620a7a4daab64eb975e3237454f6cd1c7
Min-92 <okssud1@naver.com>
2019-05-03 16:56:08 +0900

    back joon 2914 저작권

---
+++ bj2914.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/2914
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cin >> a >> b;
+
+    cout <<a*(b-1)+1;
+
+
+}

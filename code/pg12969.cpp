e4934e0ec604c5ea9b98de266a1f3e2c11f6f46e
wangmin <0201.mj.lee@gmail.com>
2019-05-25 19:04:09 +0900

    programmers 12969 직사각형 별찍기

---
+++ code/pg12969.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+
+    for(int i = 0 ; i<b; i++){
+        for(int i = 0;i < a; i++){
+            cout << '*';
+        }
+        cout <<endl;
+    }
+
+
+
+    // cout << a + b << endl;
+    return 0;
+}
